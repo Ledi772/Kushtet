66727a21d3b605ab21f355bfb5b94a9f8120aa02
Ledi772 <halilijuled2@gmail.com>
2024-11-26 08:02:27 -0800

    'test'

---
+++ script.js
@@ -0,0 +1,39 @@
+// let numri1 = prompt("shkruaj numrin e pare")
+// let numri2 = prompt("shkruaj numrin e dyte")
+
+// if ( numri1 < numri2 ) {
+//     alert("kushti ishte true")
+// }
+
+// let notaMatematike = prompt("shkruani noten ne matematike")
+// if (notaMatematike >= 2 && notaMatematike <= 5) {
+//     alert("keni kalu");
+// }
+//  else if (notaMatematike < 2) {
+//     alert("nota eshte gabim shkruani perseri");
+//     notaMatematike = prompt("shkruaj noten perseri");
+// }
+// else{
+//     alert("diqka shkoi gabim")
+// }
+// console.log
+
+
+let mosha = prompt('enter your age')
+if (mosha >=1 && mosha <=18){
+   
+    alert("jeni femi");
+}
+
+if (mosha >=18 && mosha <=65){
+   
+    alert("jeni i rritur");
+}
+
+if (mosha >=65 && mosha <=100){
+   
+    alert("i moshum");
+}
+else{
+    alert("ka diqka gabim")
+}
